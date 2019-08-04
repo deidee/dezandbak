4d99afeba2ffb0c3c55be909aba47d938ca1440d
acj <gitlab@acjs.net>
2019-08-04 21:16:48 +0200

    Set up basis for color manipulation test.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,17 @@
 'use strict';
 
-var gulp = require('gulp');
-var less = require('gulp-less');
+let gulp = require('gulp');
+let less = require('gulp-less');
+let sass = require('gulp-sass');
 
 gulp.task('less', function () {
     return gulp.src('./less/**/*.less')
         .pipe(less())
         .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(['./**/*.scss', '!node_modules/**/*'])
+        .pipe(sass())
+        .pipe(gulp.dest('./'));
 });
