37cfb0773ced8590b562443bcf3f12ad06fd1923
ACJ <gitlab@acjs.net>
2021-08-12 14:19:24 +0200

    PIL create empty image PoC

--- pillow/helloworld.py
+++ pillow/helloworld.py
@@ -1,6 +1,4 @@
 from PIL import Image
 
-print('Hello, World!')
-
-img = Image.open('177525_10150987745223956_1777701739_o.jpg')
-img.convert("CMYK").save('test.jpg')
+img = Image.new("CMYK", (480, 480))
+img.save('test.jpg')
