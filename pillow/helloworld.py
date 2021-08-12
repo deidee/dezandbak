9e0ef2400d59062120e3bf46470ffd35fd367f19
ACJ <gitlab@acjs.net>
2021-08-12 14:37:11 +0200

    Write text to image PoC

--- pillow/helloworld.py
+++ pillow/helloworld.py
@@ -1,4 +1,7 @@
 from PIL import Image
+from PIL import ImageDraw
 
 img = Image.new("CMYK", (480, 480))
+draw = ImageDraw.Draw(img)
+draw.text((0, 0),"Hello, World!",(255,0,255))
 img.save('test.pdf')
