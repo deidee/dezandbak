9f7756cdd448bad9de1fc3b93df292803dbf8b3b
ACJ <gitlab@acjs.net>
2022-12-11 10:44:11 +0100

    Add _allRGB_ script cooked up by _ChatGPT_

--- ai/allrgb.py
+++ ai/allrgb.py
@@ -1,20 +1,22 @@
 # Import the necessary libraries
+import random
 from PIL import Image
 
 # Create a new image with the appropriate dimensions
-width = 4096
+width = 4096  # The image should be 4096 by 4096 pixels
 height = 4096
 image = Image.new("RGB", (width, height))
 
-# Iterate over all possible values of red, green, and blue
-for red in range(0, 256):
-  for green in range(0, 256):
-    for blue in range(0, 256):
-      # Calculate the x and y coordinates of the current pixel
-      x = red * 16 + green // 16
-      y = green % 16 * 256 + blue
-      # Set the corresponding pixel in the image to the current color
-      image.putpixel((x, y), (red, green, blue))
+# Create a list of all possible colors
+colors = [(red, green, blue) for red in range(0, 256) for green in range(0, 256) for blue in range(0, 256)]
+
+# Shuffle the colors using a random number generator
+random.shuffle(colors)
+
+# Iterate over all pixels in the image and assign each pixel a randomly shuffled color
+for x in range(0, width):
+  for y in range(0, height):
+    image.putpixel((x, y), colors[x * width + y])
 
 # Save the image to a file
-image.save("all_colors.png")
+image.save("shuffled_colors.png")
