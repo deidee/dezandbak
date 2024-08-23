bc33e94110df731161a543a9fdc896ef4b46f022
ACJ <gitlab@acjs.net>
2024-08-23 17:51:31 +0200

    Tweak screenshot generator

--- screenshots/trying_playwright.py
+++ screenshots/trying_playwright.py
@@ -1,14 +1,20 @@
 from playwright.sync_api import sync_playwright
+from urllib.parse import urlparse
 
 def run(playwright):
+    url = 'https://www.anbi-collectief.nl/aanmelden'
+    o = urlparse(url)
+
     # launch the browser
     browser = playwright.chromium.launch()
     # opens a new browser page
     page = browser.new_page()
+    page.set_viewport_size({"width": 1280, "height": 768})
+
     # navigate to the website
-    page.goto('https://galeriehelder.nl')
+    page.goto(url)
     # take a full-page screenshot
-    page.screenshot(path='example.png', full_page=True)
+    page.screenshot(path=o.hostname + '.png', full_page=False)
     # always close the browser
     browser.close()
 
